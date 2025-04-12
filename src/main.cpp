8e428d84c32722bd7f3a69cbaa40911f9b6f0640
Fryy55 <ivan_ugu55@icloud.com>
2025-04-13 02:06:30 +0500

    Fix stuff so it at least works now

--- src/main.cpp
+++ src/main.cpp
@@ -20,34 +20,69 @@ class $modify(HCreatorLayer, CreatorLayer) {
 		button -> toggle(locked);
 		button -> setID("quests-toggle"_spr);
 
+		auto menu = this -> getChildByID("top-right-menu");
+		menu -> addChild(button);
+		menu -> updateLayout();
+
 		if (locked) {
 			Mod::get() -> setSavedValue<bool>("locked", false);
 			onQuestsToggle(nullptr);
 		}
 
-		auto menu = this -> getChildByID("top-right-menu");
-		menu -> addChild(button);
-		menu -> updateLayout();
-
+		//this -> querySelector("")
 		return true;
+	}
+
+	void checkQuestsStatus() {
+		CreatorLayer::checkQuestsStatus();
+
+		if (m_questsSprite -> getChildrenCount()) {
+			m_questsSprite -> removeAllChildren();
+			addExMark(m_questsSprite, Mod::get() -> getSavedValue("locked", false));
+		}
+
+		return;
 	}
 
 	void onQuestsToggle(CCObject* sender) {
 		bool locked = !Mod::get() -> getSavedValue<bool>("locked", false);
 		Mod::get() -> setSavedValue<bool>("locked", locked);
 
-		auto questsButton = static_cast<CCMenuItemSpriteExtra*>(
-			this -> getChildByID("creator-buttons-menu") -> getChildByID("quests-button")
-		);
-		auto sprite =(
+		auto questsButton = static_cast<CCMenuItemSpriteExtra*>(m_questsSprite -> m_pParent);
+		auto sprite = (
 			locked ?
 				CCSpriteGrayscale::createWithSpriteFrameName("GJ_challengeBtn_001.png")
 			:
 				CCSprite::createWithSpriteFrameName("GJ_challengeBtn_001.png")
 		);
 		sprite -> setScale(0.8f);
-		questsButton -> setSprite(sprite);
+		sprite -> setCascadeColorEnabled(true);
+		sprite -> setColor({175, 175, 175});
+
+		if (m_questsSprite -> getChildrenCount()) {
+			addExMark(sprite, locked);
+
+			m_questsSprite -> removeAllChildren();
+		}
+
+		questsButton -> removeAllChildren();
+		questsButton -> addChildAtPosition(sprite, Anchor::Center);
 		questsButton -> setEnabled(!locked);
+		m_questsSprite = sprite;
+
+		return;
+	}
+
+	void addExMark(CCSprite* sprite, bool locked) {
+		auto exMark = (
+			locked ?
+				CCSpriteGrayscale::createWithSpriteFrameName("exMark_001.png")
+			:
+				CCSprite::createWithSpriteFrameName("exMark_001.png")
+		);
+		sprite -> addChild(exMark);
+		exMark -> setPosition({20.f, 83.6f});
+		exMark -> setScale(0.8f);
 
 		return;
 	}
