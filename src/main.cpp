b9649bac59a6f0bd44d7d17c8d467f1566e054de
Fryy55 <ivan_ugu55@icloud.com>
2025-04-15 01:57:06 +0500

    v1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CreatorLayer.hpp>
+#include <Geode/modify/CCMenuItemSpriteExtra.hpp>
 
 using namespace geode::prelude;
 
@@ -10,46 +11,62 @@ class $modify(HCreatorLayer, CreatorLayer) {
 		CCSprite* m_lockedQuestsExMarkSprite;
 	};
 
-	bool init() {
+	$override bool init() {
 		if (!CreatorLayer::init())
 			return false;
 
 		bool locked = Mod::get() -> getSavedValue<bool>("locked", false);
 
 		// Toggle button
-		auto button = CCMenuItemToggler::create(
+		auto lockButton = CCMenuItemToggler::create(
 			CCSprite::createWithSpriteFrameName("GJ_lock_open_001.png"),
 			CCSprite::createWithSpriteFrameName("GJ_lock_001.png"),
 			this,
 			menu_selector(HCreatorLayer::onQuestsToggle)
 		);
-		button -> toggle(locked);
-		// TODO: values below
-		button -> setPosition({});
-		button -> setScale();
-		button -> setID("quests-toggle"_spr);
-		
+		auto lockMenu = CCMenu::create();
+
+		lockButton -> toggle(locked);
+		auto offSpr = lockButton -> m_offButton -> getChildByType<CCSprite>(0);
+		offSpr -> setPositionY(offSpr -> getPositionY() + 3.f);
+		lockButton -> setID("quests-toggle"_spr);
+		lockMenu -> setContentSize({30.f, 30.f});
+		lockMenu -> setScale(0.75f);
+		lockMenu -> setID("quests-toggle-menu"_spr);
+
+		lockMenu -> addChildAtPosition(lockButton, Anchor::Center);
+
 		// Locked sprite
 		auto sprite = CCSprite::createWithSpriteFrameName("GJ_challengeBtn_001.png");
 		auto exMark = CCSprite::createWithSpriteFrameName("exMark_001.png");
 
+		sprite -> addChild(exMark);
+
 		sprite -> setScale(0.8f);
-		sprite -> setCascadeColor(true);
+		sprite -> setCascadeColorEnabled(true);
 		sprite -> setColor(ccGRAY);
+		sprite -> setVisible(false);
 		sprite -> setID("locked-sprite"_spr);
 		exMark -> setPosition({20.f, 83.6f});
 		exMark -> setScale(0.8f);
 		exMark -> setID("ex-mark-locked-sprite"_spr);
 
-		sprite -> addChild(exMark);
-
 		m_fields -> m_lockedQuestsSprite = sprite;
 		m_fields -> m_lockedQuestsExMarkSprite = exMark;
 
+		// Put stuff together
 		auto questsButton = static_cast<CCMenuItemSpriteExtra*>(m_questsSprite -> m_pParent);
-		sprite -> setVisible(false);
 		questsButton -> addChildAtPosition(sprite, Anchor::Center);
-		questsButton -> addChild(button);
+
+		this -> addChild(lockMenu);
+		auto buttonsMenu = this -> getChildByID("creator-buttons-menu");
+		lockMenu -> setPosition(
+			(buttonsMenu -> getPosition() - buttonsMenu -> getContentSize() / 2.f)
+			+
+			(questsButton -> getPosition() - questsButton -> getContentSize() / 2.f)
+			+
+			CCPoint(10.f, 10.f)
+		);
 
 		// Lock if locked waow
 		if (locked) {
@@ -60,7 +77,6 @@ class $modify(HCreatorLayer, CreatorLayer) {
 		return true;
 	}
 
-
 	void onQuestsToggle(CCObject* sender) {
 		bool locked = !Mod::get() -> getSavedValue<bool>("locked", false);
 		Mod::get() -> setSavedValue<bool>("locked", locked);
@@ -69,6 +85,38 @@ class $modify(HCreatorLayer, CreatorLayer) {
 		m_questsSprite -> setVisible(!locked);
 		m_fields -> m_lockedQuestsExMarkSprite -> setVisible(m_questsSprite -> getChildrenCount());
 		static_cast<CCMenuItemSpriteExtra*>(m_questsSprite -> m_pParent) -> setEnabled(!locked);
+
+		return;
+	}
+};
+
+class $modify(CCMenuItemSpriteExtra) {
+	struct Fields {
+		bool m_isQuests = false; // "My child will initialize fields in init"
+		bool m_init = false;
+	};
+
+	$override void selected() {
+		CCMenuItemSpriteExtra::selected();
+
+		if (!(m_fields -> m_init)) {
+			m_fields -> m_isQuests = (this -> getID() == "quests-button");
+			m_fields -> m_init = true;
+		}
+
+		if (m_fields -> m_isQuests)
+			this -> m_pParent -> m_pParent -> getChildByID("quests-toggle-menu"_spr) ->
+				getChildByType<CCMenuItemToggler>(0) -> m_offButton -> runAction(CCFadeOut::create(0.3f));
+
+		return;
+	}
+
+	$override void unselected() {
+		CCMenuItemSpriteExtra::unselected();
+
+		if (m_fields -> m_isQuests)
+			this -> m_pParent -> m_pParent -> getChildByID("quests-toggle-menu"_spr) ->
+				getChildByType<CCMenuItemToggler>(0) -> m_offButton -> runAction(CCFadeIn::create(0.3f));
 
 		return;
 	}
